e7fe145db7ff6f14239e463daa32c6ee0228c684
testuser <testuser@example.com>
2023-01-19 08:11:26 +0530

    added a shell script

---
+++ user_input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Enter Current Year (YYYY): " input_year_val
+current_year=`date +'%Y'`
+echo $current_year
+if test $input_year_val == $current_year
+then
+    echo "You have entered the correct Year"
+fi
+if test $input_year_val != $current_year
+then
+    echo "You have not entered the Current Year. Please try again."
+fi
